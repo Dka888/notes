701735adf1eaeea20f1e95cd0a22ca0bfa372a9f
Dmitrij Kosow <dymitrkosow@gmail.com>
2023-11-18 08:00:39 +0100

    db

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-const password = process.env.DB_PASSWORD;
-const username = process.env.DB_USERNAME
-const databaseName = process.env.DB_NAME;
-const host = process.env.DB_HOST;
+// const password = process.env.DB_PASSWORD;
+// const username = process.env.DB_USERNAME
+// const databaseName = process.env.DB_NAME;
+// const host = process.env.DB_HOST;
 
-export const sequelize = new Sequelize(username, databaseName, password, {
-  host,
-  dialect: 'postgres',
-  logging: false,
-});
+// export const sequelize = new Sequelize(username, databaseName, password, {
+//   host,
+//   dialect: 'postgres',
+//   logging: false,
+// });
 
-//  const database = process.env.POSTGRESS_URL + '?sslmode=require';
+ const database = process.env.POSTGRESS_URL + '?sslmode=require';
 
-// export const sequelize = new Sequelize(database);
+export const sequelize = new Sequelize(database);
 
 sequelize
   .authenticate()
