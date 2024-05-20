41c76b034ea19df33f9210e815d936bf6745a579
Dymitr <dymitrkosow@gmail.com>
2024-05-20 11:02:17 +0200

    calnedar

--- note/src/components/Home/Home.tsx
+++ note/src/components/Home/Home.tsx
@@ -2,23 +2,23 @@ import { useState, useCallback } from "react";
 import { Header } from "../Header/Header";
 import { Board } from "../Board/Board";
 import { Navbar } from "../Navbar/Navbar";
-// import { useNoteContext } from "../../context/Context";
-// import { Login } from "../Authorization/Login/Login";
+import { useNoteContext } from "../../context/Context";
+import { Login } from "../Authorization/Login/Login";
 
 export const Home = () => {
     const [expanded, setExpanded] = useState(false);
     const [expandPermanent, setExpandPermanent] = useState(false);
     // const [showLogin, setShowLogin] = useState(true)
 
-    // const { isLogin } = useNoteContext();
+    const { isLogin } = useNoteContext();
 
     const toggleExpand = useCallback(() => {
         setExpanded(!expanded);
     }, [expanded]);
 
-    // if (!isLogin) {
-    //     <Login />
-    // }
+    if (!isLogin) {
+        <Login />
+    }
 
     return (
         <>
